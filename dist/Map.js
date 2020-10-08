09ed0535fbe408484b467bea9aa541a5b03b8b47
Michael <michael.speitelsbach@gmail.com>
2020-10-09 00:22:34 +0200

    added zooming

--- dist/Map.js
+++ dist/Map.js
@@ -5,13 +5,23 @@ var util = require("./util");
 var DirectoryBox_1 = require("./DirectoryBox");
 var Map = /** @class */ (function () {
     function Map() {
-        util.setContent('<div id="map""></div>');
-        this.rootDirectory = new DirectoryBox_1.DirectoryBox(null, './src', 'map');
+        var _this = this;
+        this.scalePercent = 100;
+        util.setContent('<div id="map" style="overflow:hidden; width:100%; height:100%"></div>');
+        util.setContentTo('map', '<div id="mapRatioAdjuster"></div>');
+        util.setContentTo('mapRatioAdjuster', '<div id="root"></div>');
+        this.updateStyle();
+        this.rootDirectory = new DirectoryBox_1.DirectoryBox(null, './src', 'root');
         this.rootDirectory.render(99, 99);
-        util.addWheelListenerTo('map', this.zoom);
+        util.addWheelListenerTo('map', function (delta) { return _this.zoom(delta); });
     }
     Map.prototype.zoom = function (delta) {
-        util.logInfo("zooming " + delta);
+        this.scalePercent -= this.scalePercent * (delta / 1000);
+        util.logInfo('zoom: ' + this.scalePercent);
+        this.updateStyle();
+    };
+    Map.prototype.updateStyle = function () {
+        util.setStyleTo('mapRatioAdjuster', 'width:' + this.scalePercent + '%;height:' + this.scalePercent + '%;');
     };
     return Map;
 }());
--- src/Map.ts
+++ src/Map.ts
@@ -3,18 +3,28 @@ import { DirectoryBox } from './DirectoryBox'
 
 export class Map {
   private rootDirectory: DirectoryBox
+  private scalePercent: number = 100
 
   public constructor() {
-    util.setContent('<div id="map""></div>')
+    util.setContent('<div id="map" style="overflow:hidden; width:100%; height:100%"></div>')
+    util.setContentTo('map', '<div id="mapRatioAdjuster"></div>')
+    util.setContentTo('mapRatioAdjuster', '<div id="root"></div>')
+    this.updateStyle()
 
-    this.rootDirectory = new DirectoryBox(null, './src', 'map')
+    this.rootDirectory = new DirectoryBox(null, './src', 'root')
     this.rootDirectory.render(99, 99)
 
-    util.addWheelListenerTo('map', this.zoom)
+    util.addWheelListenerTo('map', (delta) => this.zoom(delta))
   }
 
   private zoom(delta: number): void {
-    util.logInfo("zooming " + delta)
+    this.scalePercent -= this.scalePercent * (delta/1000)
+    util.logInfo('zoom: ' + this.scalePercent)
+    this.updateStyle()
+  }
+
+  private updateStyle() {
+    util.setStyleTo('mapRatioAdjuster', 'width:' + this.scalePercent + '%;height:' + this.scalePercent + '%;')
   }
 
 }
