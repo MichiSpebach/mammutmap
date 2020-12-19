fb95f08ce944c51267aaa5824c8722dd0bc019af
Michael <michael.speitelsbach@gmail.com>
2020-12-19 13:44:04 +0100

    refactor

--- src/DragManager.ts
+++ src/DragManager.ts
@@ -6,22 +6,20 @@ import { DirectoryBox } from './box/DirectoryBox'
 // TODO: rename to BoxDragManager?
 export class DragManager {
 
-  private static draggingBox: Box|null = null // TODO: refactor either both are null or none
-  private static dragOverBox: DirectoryBox|null = null
+  private static state: {
+    dragging: Box
+    draggingOver: DirectoryBox
+  } | null
 
-  private static setDragOverBox(box: DirectoryBox|null): void {
-    if (this.dragOverBox == box) {
-      return
+  private static setState(newState: {dragging: Box, draggingOver: DirectoryBox} | null): void {
+    if (this.state != null) {
+      this.state.draggingOver.setDragOverStyle(false)
+    }
+    if (newState != null) {
+      newState.draggingOver.setDragOverStyle(true)
     }
 
-    if (this.dragOverBox != null) {
-      this.dragOverBox.setDragOverStyle(false)
-    }
-    if (box != null) {
-      box.setDragOverStyle(true)
-    }
-
-    this.dragOverBox = box
+    this.state = newState
   }
 
   public static addDraggable(elementToDrag: Box): void {
@@ -29,35 +27,39 @@ export class DragManager {
 
     dom.addDragListenerTo(draggableId, 'dragstart', (clientX: number, clientY: number) => {
       elementToDrag.dragStart(clientX, clientY)
-      this.draggingBox = elementToDrag
-      this.setDragOverBox(elementToDrag.getParent())
+      this.setState({dragging: elementToDrag, draggingOver: elementToDrag.getParent()})
     })
 
     dom.addDragListenerTo(draggableId, 'drag', (clientX: number, clientY: number) => {
-      if (this.dragOverBox == null) {
-        util.logWarning("DragManager: dragOverBox is null although dragging is in progress")
+      if (this.state == null) {
+        util.logWarning("DragManager: state is null although dragging is in progress")
         elementToDrag.dragCancel()
         return
       }
-
-      elementToDrag.drag(clientX, clientY, this.dragOverBox)
+      elementToDrag.drag(clientX, clientY, this.state.draggingOver)
     })
 
-    dom.addDragListenerTo(draggableId, 'dragend', (clientX: number, clientY: number) => {
-      if (this.dragOverBox == null) {
-        util.logWarning("DragManager: dragOverBox is null although dragging was in progress")
+    dom.addDragListenerTo(draggableId, 'dragend', (_) => {
+      if (this.state == null) {
+        util.logWarning("DragManager: state is null although dragging was in progress")
         elementToDrag.dragCancel()
         return
       }
-      // TODO: call elementToDrag.dragCancel() if esc is pressed
       elementToDrag.dragEnd()
-      this.draggingBox = null
-      this.setDragOverBox(null)
+      this.setState(null)
     })
+
+    // TODO: call elementToDrag.dragCancel() if esc is pressed
   }
 
   public static addDropTarget(targetElement: DirectoryBox): void {
-    dom.addDragListenerTo(targetElement.getId(), 'dragenter', (_) => this.setDragOverBox(targetElement))
+    dom.addDragListenerTo(targetElement.getId(), 'dragenter', (_) => {
+      if (this.state == null) {
+        util.logWarning("DragManager: state is null although dragging is in progress")
+        return
+      }
+      this.setState({dragging: this.state.dragging, draggingOver: targetElement})
+    })
   }
 
 }
