471700c214c2cbf3c8284af45687eaab312af632
Michael <michael.speitelsbach@gmail.com>
2021-01-19 20:56:06 +0100

    fix positioning of links

--- src/box/Link.ts
+++ src/box/Link.ts
@@ -44,7 +44,7 @@ export class Link {
   }
 
   public async renderStyle(): Promise<void> {
-    return dom.setStyleTo(this.data.id, 'position:absolute;width:100%;height:100%;pointer-events:none;')
+    return dom.setStyleTo(this.data.id, 'position:absolute;top:0;width:100%;height:100%;pointer-events:none;')
   }
 
 }
