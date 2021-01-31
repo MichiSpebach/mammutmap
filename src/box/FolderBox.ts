ba5ad1d687fe8ebe088f3e0c02be64405b3ba5d1
Michael <michael.speitelsbach@gmail.com>
2021-01-31 18:18:10 +0100

    refactor

--- src/box/FolderBox.ts
+++ src/box/FolderBox.ts
@@ -6,61 +6,16 @@ import { FolderBoxHeader } from './FolderBoxHeader'
 import { FolderBoxBody } from './FolderBoxBody'
 import { Link } from './Link'
 import { BoxMapLinkData } from './BoxMapLinkData'
-import { WayPoint } from './WayPoint'
 import { DragManager } from '../DragManager'
 
 export class FolderBox extends Box {
   private dragOver: boolean = false
   private readonly body: FolderBoxBody
-  private links: Link[]
+  private links: Link[] = []
 
   public constructor(name: string, parent: FolderBox|null, mapData: BoxMapData, mapDataFileExists: boolean) {
     super(name, parent, mapData, mapDataFileExists)
     this.body = new FolderBoxBody(this)
-
-    if (name == 'box' && !this.getMapLinkData().find(linkData => linkData.toWayPoints[0].boxId === '93kjm246u28')) { // TODO: remove
-      const fromWayPoint = new WayPoint('iu4msv07ut8', 100, 50)
-      const toWayPoint = new WayPoint('93kjm246u28', 0, 50)
-      const linkData = new BoxMapLinkData(util.generateId(), [fromWayPoint], [toWayPoint])
-      this.getMapLinkData().push(linkData)
-      this.saveMapData()
-      util.logInfo('add link to box and save')
-    }
-    if (name == 'box' && !this.getMapLinkData().find(linkData => linkData.toWayPoints[0].boxId === '3n81ck4i5sg')) { // TODO: remove
-      const fromWayPoint = new WayPoint('iu4msv07ut8', 50, 100)
-      const toWayPoint = new WayPoint('3n81ck4i5sg', 50, 0)
-      const linkData = new BoxMapLinkData(util.generateId(), [fromWayPoint], [toWayPoint])
-      this.getMapLinkData().push(linkData)
-      util.logInfo('add link to box and save')
-    }
-    if (name == 'box' && !this.getMapLinkData().find(linkData => linkData.toWayPoints[0].boxId === 'ujltrbp3r9')) { // TODO: remove
-      const fromWayPoint = new WayPoint('iu4msv07ut8', 100, 50)
-      const toWayPoint = new WayPoint('ujltrbp3r9', 0, 50)
-      const linkData = new BoxMapLinkData(util.generateId(), [fromWayPoint], [toWayPoint])
-      this.getMapLinkData().push(linkData)
-      util.logInfo('add link to box and save')
-    }
-    if (name == 'box' && !this.getMapLinkData().find(linkData => linkData.toWayPoints[0].boxId === 'flo206thmv')) { // TODO: remove
-      const fromWayPoint = new WayPoint('93kjm246u28', 100, 25)
-      const toWayPoint = new WayPoint('flo206thmv', 0, 50)
-      const linkData = new BoxMapLinkData(util.generateId(), [fromWayPoint], [toWayPoint])
-      this.getMapLinkData().push(linkData)
-      util.logInfo('add link to box and save')
-    }
-    if (name == 'box' && !this.getMapLinkData().find(linkData => linkData.toWayPoints[0].boxId === 'qm6p7cuvvl')) { // TODO: remove
-      const fromWayPoint = new WayPoint('flo206thmv', 50, 100)
-      const toWayPoint = new WayPoint('qm6p7cuvvl', 50, 0)
-      const linkData = new BoxMapLinkData(util.generateId(), [fromWayPoint], [toWayPoint])
-      this.getMapLinkData().push(linkData)
-      this.saveMapData()
-      util.logInfo('add link to box and save')
-    }
-
-    this.links = []
-    this.getMapLinkData().forEach((linkData: BoxMapLinkData) => {
-      this.links.push(new Link(linkData, this))
-    })
-
   }
 
   protected createHeader(): FolderBoxHeader {
@@ -105,6 +60,10 @@ export class FolderBox extends Box {
   }
 
   private async renderLinks(): Promise<void> {
+    this.getMapLinkData().forEach((linkData: BoxMapLinkData) => {
+      this.links.push(new Link(linkData, this))
+    })
+
     await Promise.all(this.links.map(async (link: Link) => {
       await link.render()
     }))
