cba56321e3dedf905e7f8c043991a104f720301a
Michael <michael.speitelsbach@gmail.com>
2021-08-07 14:11:29 +0200

    LinkGenerator prints imports of index.ts

--- plugin-src/TypeScriptLinkGenerator.ts
+++ plugin-src/TypeScriptLinkGenerator.ts
@@ -1,7 +1,29 @@
+import * as ts from 'typescript'
+import { Program, SourceFile } from 'typescript'
 import { MenuItem } from 'electron'
 import * as util from '../dist/util'
 import * as applicationMenu from '../dist/applicationMenu'
 
-util.logInfo('hello world')
-
+applicationMenu.addMenuItemTo('TypeScriptLinkGenerator.js', new MenuItem({label: 'Generate links', click: generateLinks}))
 applicationMenu.addMenuItemTo('TypeScriptLinkGenerator.js', new MenuItem({label: 'Join on GitHub (coming soon)'}))
+
+function generateLinks(): void {
+  util.logInfo('generateLinks')
+
+  const rootFilePath: string = 'src/index.ts'
+  const program: Program = ts.createProgram([rootFilePath], {})
+
+  const sourceFile: SourceFile|undefined = program.getSourceFile(rootFilePath)
+  if (!sourceFile) {
+    util.logError('failed to get '+ rootFilePath +' as SourceFile')
+    return // TODO: compiler does not know that util.logError(..) returns never
+  }
+
+  ts.forEachChild(sourceFile, node => {
+    if (ts.isImportDeclaration(node)) {
+      util.logInfo(node.moduleSpecifier.getText(sourceFile))
+    }
+  })
+
+  util.logInfo('generateLinks finished')
+}
--- plugin/TypeScriptLinkGenerator.js
+++ plugin/TypeScriptLinkGenerator.js
@@ -1,7 +1,24 @@
 "use strict";
 exports.__esModule = true;
+var ts = require("typescript");
 var electron_1 = require("electron");
 var util = require("../dist/util");
 var applicationMenu = require("../dist/applicationMenu");
-util.logInfo('hello world');
+applicationMenu.addMenuItemTo('TypeScriptLinkGenerator.js', new electron_1.MenuItem({ label: 'Generate links', click: generateLinks }));
 applicationMenu.addMenuItemTo('TypeScriptLinkGenerator.js', new electron_1.MenuItem({ label: 'Join on GitHub (coming soon)' }));
+function generateLinks() {
+    util.logInfo('generateLinks');
+    var rootFilePath = 'src/index.ts';
+    var program = ts.createProgram([rootFilePath], {});
+    var sourceFile = program.getSourceFile(rootFilePath);
+    if (!sourceFile) {
+        util.logError('failed to get ' + rootFilePath + ' as SourceFile');
+        return; // TODO: compiler does not know that util.logError(..) returns never
+    }
+    ts.forEachChild(sourceFile, function (node) {
+        if (ts.isImportDeclaration(node)) {
+            util.logInfo(node.moduleSpecifier.getText(sourceFile));
+        }
+    });
+    util.logInfo('generateLinks finished');
+}
