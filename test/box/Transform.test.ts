0c669db04fcbb080431b5af02d080d533b284831
Michael <michael.speitelsbach@gmail.com>
2022-01-06 18:02:34 +0100

    Revert "fix tests"
    This reverts commit f9ffc6d93209f4d7403a2c2fa7287179e9f7a8de.

--- test/box/Transform.test.ts
+++ test/box/Transform.test.ts
@@ -13,21 +13,21 @@ test('localToClientPosition', async () => {
 test('getNearestGridPositionOfOtherTransform', async () => {
   const scenario = setupScenario()
   const result: LocalPosition = await setupScenario().transform.getNearestGridPositionOfOtherTransform(new ClientPosition(609, 346), scenario.otherTransform)
-  expect(result.percentX).toBeCloseTo(26.5, 10)
-  expect(result.percentY).toBeCloseTo(23, 10)
+  expect(result.percentX).toBeCloseTo(27.5, 10)
+  expect(result.percentY).toBeCloseTo(22.5, 10)
 })
 
-test('getNearestGridPositionOf rounds to multiple of 4', () => {
-  const result: LocalPosition = setupScenario().transform.getNearestGridPositionOf(new LocalPosition(3, 78))
-  expect(result.percentX).toBe(4)
-  expect(result.percentY).toBe(80)
+test('getNearestGridPositionOf rounds to multiple of 5', () => {
+  const result: LocalPosition = setupScenario().transform.getNearestGridPositionOf(new LocalPosition(3, 67))
+  expect(result.percentX).toBe(5)
+  expect(result.percentY).toBe(65)
 })
 
 function setupScenario(): {transform: Transform, otherTransform: Transform} {
   const box: FolderBox = new FolderBox('src/box', null, mock<BoxMapData>(), false)
   box.getClientRect = () => Promise.resolve(new Rect(500, 300, 400, 200))
   const otherBox: FolderBox = new FolderBox('src/box/other', null, mock<BoxMapData>(), false)
-  otherBox.getClientRect = () => Promise.resolve(new Rect(550, 350, 200, 100))
+  otherBox.getClientRect = () => Promise.resolve(new Rect(500, 300, 200, 100))
 
   return {transform: box.transform, otherTransform: otherBox.transform}
 }
