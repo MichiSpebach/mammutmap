400706b83389a53e78fcbe05afb4ec32eca8858a
Michael <michael.speitelsbach@gmail.com>
2022-01-18 01:32:41 +0100

    add test for merging into json files

--- test/fileSystemAdapter.test.ts
+++ test/fileSystemAdapter.test.ts
@@ -4,3 +4,27 @@ test('doesDirentExist', async () => {
   expect(await fileSystem.doesDirentExist('src')).toBe(true)
   expect(await fileSystem.doesDirentExist('notExisting')).toBe(false)
 })
+
+test('mergeObjectIntoJsonFile', async () => {
+  let originalJson: string = '{'
+    + '"name": "box1",'
+    + '"x": 1'
+    + '}'
+  fileSystem.readFile = (path: string): Promise<string> => {
+    return Promise.resolve(originalJson)
+  }
+  let savedJson: string = ''
+  fileSystem.writeFile = (path: string, data: string): Promise<void> => {
+    savedJson = data
+    return Promise.resolve()
+  }
+
+  let object: Object = {name: "box2", x: 3}
+
+  await fileSystem.mergeObjectIntoJsonFile('path', object)
+
+  expect(savedJson).toBe('{\n'
+    + '\t"name": "box2",\n'
+    + '\t"x": 3\n'
+    + '}')
+})
