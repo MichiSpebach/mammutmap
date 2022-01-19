4ed8fb0a7570b28deab1597ce62ce3a68f460374
Michael <michael.speitelsbach@gmail.com>
2022-01-19 01:23:19 +0100

    add tests

--- test/fileSystemAdapter.test.ts
+++ test/fileSystemAdapter.test.ts
@@ -6,25 +6,80 @@ test('doesDirentExist', async () => {
 })
 
 test('mergeObjectIntoJsonFile', async () => {
-  let originalJson: string = '{'
+  mockReadFile('{'
     + '"name": "box1",'
     + '"x": 1'
-    + '}'
-  fileSystem.readFile = (path: string): Promise<string> => {
-    return Promise.resolve(originalJson)
-  }
-  let savedJson: string = ''
-  fileSystem.writeFile = (path: string, data: string): Promise<void> => {
-    savedJson = data
-    return Promise.resolve()
-  }
+    + '}')
+  const getSavedData: () => string = mockWriteFile().getSavedData
 
-  let object: Object = {name: "box2", x: 3}
+  await fileSystem.mergeObjectIntoJsonFile('path', {name: "box2", x: 3})
 
-  await fileSystem.mergeObjectIntoJsonFile('path', object)
-
-  expect(savedJson).toBe('{\n'
+  expect(getSavedData()).toBe('{\n'
     + '\t"name": "box2",\n'
     + '\t"x": 3\n'
     + '}')
 })
+
+test('mergeObjectIntoJsonFile partial', async () => {
+  mockReadFile('{'
+    + '"id": "box1",'
+    + '"x": 1'
+    + '}')
+  const getSavedData: () => string = mockWriteFile().getSavedData
+
+  await fileSystem.mergeObjectIntoJsonFile('path', {x: 2})
+
+  expect(getSavedData()).toBe('{\n'
+    + '\t"id": "box1",\n'
+    + '\t"x": 2\n'
+    + '}')
+})
+
+test('mergeObjectIntoJsonFile additional', async () => {
+  mockReadFile('{'
+    + '"id": "box1"'
+    + '}')
+  const getSavedData: () => string = mockWriteFile().getSavedData
+
+  await fileSystem.mergeObjectIntoJsonFile('path', {x: 2})
+
+  expect(getSavedData()).toBe('{\n'
+    + '\t"id": "box1",\n'
+    + '\t"x": 2\n'
+    + '}')
+})
+
+test('mergeObjectIntoJsonFile deep', async () => {
+  mockReadFile('{'
+    + '"id": "box1",'
+    + '"profile": {'
+    + '"hash": "h4sH"'
+    + '}'
+    + '}')
+  const getSavedData: () => string = mockWriteFile().getSavedData
+
+  await fileSystem.mergeObjectIntoJsonFile('path', {profile: {loc: 123}})
+
+  expect(getSavedData()).toBe('{\n'
+    + '\t"id": "box1",\n'
+    + '\t"profile": {\n'
+//    + '\t\t"hash": "h4sH",\n' // TODO: make this work, inner objects should be merged too
+    + '\t\t"loc": 123\n'
+    + '\t}\n'
+    + '}')
+})
+
+function mockReadFile(returnData: string): void {
+  fileSystem.readFile = (_: string): Promise<string> => {
+    return Promise.resolve(returnData)
+  }
+}
+
+function mockWriteFile(): {getSavedData: (() => string)} {
+  let savedData: string
+  fileSystem.writeFile = (_: string, data: string): Promise<void> => {
+    savedData = data
+    return Promise.resolve()
+  }
+  return {getSavedData: () => savedData}
+}
