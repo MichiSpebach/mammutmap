22baead972802efa030240d846b51b6e2a85dd40
MichiSpebach <michi.spebach@gmail.com>
2022-06-16 17:57:36 +0200

    fix bug that rendered scaleTool into box that becomes unrendered and let to unrender of scaleTool itself

--- src/box/Box.ts
+++ src/box/Box.ts
@@ -34,6 +34,7 @@ export abstract class Box implements DropTarget, Hoverable {
   public readonly links: BoxLinks // TODO: rename to managedLinks?
   public readonly borderingLinks: BorderingLinks
   private rendered: boolean = false
+  private unrenderInProgress: boolean = false
   private watchers: BoxWatcher[] = []
   private unsavedChanges: boolean = false
 
@@ -257,6 +258,7 @@ export abstract class Box implements DropTarget, Hoverable {
       }
       util.logWarning('unrendering box that has watchers, this can happen when folder gets closed while plugins are busy or plugins don\'t clean up')
     }
+    this.unrenderInProgress = true
 
     DragManager.removeDropTarget(this)
     HoverManager.removeHoverable(this)
@@ -271,7 +273,8 @@ export abstract class Box implements DropTarget, Hoverable {
     proms.push(this.unrenderAdditional())
     await Promise.all(proms)
 
-  this.rendered = false
+    this.unrenderInProgress = false
+    this.rendered = false
     return {rendered: false}
   }
 
@@ -287,7 +290,7 @@ export abstract class Box implements DropTarget, Hoverable {
   }
 
   private onHoverOver(): void {
-    if (scaleTool.isScalingInProgress()) {
+    if (scaleTool.isScalingInProgress() || this.unrenderInProgress) {
       return
     }
     scaleTool.renderInto(this)
@@ -351,6 +354,10 @@ export abstract class Box implements DropTarget, Hoverable {
   }
 
   public async attachGrid(priority: RenderPriority = RenderPriority.NORMAL): Promise<void> {
+    if (this.unrenderInProgress) {
+      util.logWarning('prevented attaching grid to box that gets unrendered') // TODO: only to check that this gets triggered, remove
+      return
+    }
     await grid.renderInto(this.getGridPlaceHolderId(), priority)
   }
 
