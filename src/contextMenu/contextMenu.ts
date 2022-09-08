464cad74d4dfbcfa205c18405e9f78d35a787df1
Michael <michi.spebach@gmail.com>
2022-09-08 23:14:57 +0200

    update todo list and fix double displaying of already included tags

--- src/contextMenu/contextMenu.ts
+++ src/contextMenu/contextMenu.ts
@@ -116,20 +116,27 @@ function buildAddNodeItem(box: Box, clientX: number, clientY: number): MenuItemF
 }
 
 function buildTagLinkItemFolder(link: Link): MenuItemFolder {
-  const items: MenuItem[] = [
-    buildTagLinkItem(link, 'hidden'),
-    buildTagLinkItem(link, 'isA'), // TODO: add description "inheritance"
-    buildTagLinkItem(link, 'has'), // TODO: add description "composition"
-    buildTagLinkItem(link, 'important'), // TODO: add description "visible although other tag would hide it"
-    buildTagLinkItem(link, 'falsePositive'), // TODO: add description "wrongly recognized by plugin"
-    buildAddOtherTagLinkItem(link)
+  const defaultTags: string[] = [
+    'hidden',
+    'isA', // TODO: add description "inheritance"
+    'has', // TODO: add description "composition"
+    'important', // TODO: add description "visible although other tag would hide it"
+    'falsePositive', // TODO: add description "wrongly recognized by plugin"
   ]
 
-  for (const includedTag of link.getTags()) {
-    if (!items.find(item => item.label === includedTag)) {
+  const items: MenuItem[] = []
+
+  for (const includedTag of link.getTags().reverse()) { // reverse to display latest on top
+    if (!defaultTags.find(defaultTag => defaultTag === includedTag)) {
       items.unshift(buildTagLinkItem(link, includedTag))
     }
   }
+
+  for (const defaultTag of defaultTags) {
+    items.push(buildTagLinkItem(link, defaultTag))
+  }
+
+  items.push(buildAddOtherTagLinkItem(link))
 
   return new MenuItemFolder({label: 'tag', submenu: items})
 }
