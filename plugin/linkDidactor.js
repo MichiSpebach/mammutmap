703f01986f105b6cc0b382157833c4cf1d2ccdca
Michael <michi.spebach@gmail.com>
2022-09-13 22:28:48 +0200

    improve overriding for plugins

--- plugin/linkDidactor.js
+++ plugin/linkDidactor.js
@@ -8,25 +8,21 @@ const activateMenuItem = new pluginFacade_1.MenuItemFile({ label: 'activate', cl
 pluginFacade_1.applicationMenu.addMenuItemTo('linkDidactor.js', deactivateMenuItem);
 pluginFacade_1.applicationMenu.addMenuItemTo('linkDidactor.js', activateMenuItem);
 async function deactivate() {
-    DidactedLink.deactivateAndPlugout();
+    (0, Link_1.override)(DidactedLink.getSuperClass());
     await pluginFacade_1.applicationMenu.setMenuItemEnabled(deactivateMenuItem, false);
     await pluginFacade_1.applicationMenu.setMenuItemEnabled(activateMenuItem, true);
     util_1.util.logInfo('deactivated linkDidactor plugin');
 }
 async function activate() {
-    DidactedLink.activateAndPlugin();
+    (0, Link_1.override)(DidactedLink);
     await pluginFacade_1.applicationMenu.setMenuItemEnabled(deactivateMenuItem, true);
     await pluginFacade_1.applicationMenu.setMenuItemEnabled(activateMenuItem, false);
     util_1.util.logInfo('activated linkDidactor plugin');
 }
 const colors = ['green', 'blue', 'yellow', 'orange', 'magenta', 'aqua', 'lime', 'purple', 'teal'];
-class DidactedLink extends Link_1.Link {
-    static activateAndPlugin() {
-        this.getColorBackup = Link_1.Link.prototype.getColor;
-        Link_1.Link.prototype.getColor = DidactedLink.prototype.getColor;
-    }
-    static deactivateAndPlugout() {
-        Link_1.Link.prototype.getColor = DidactedLink.getColorBackup;
+class DidactedLink extends Link_1.LinkImplementation {
+    static getSuperClass() {
+        return Object.getPrototypeOf(DidactedLink.prototype).constructor;
     }
     getColor() {
         let toBoxId;
