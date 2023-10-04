01a44b5d0bd99c500b5c724024937690416b4548
Artur Schulz <artur.schulz@andrena.de>
2023-10-04 17:54:26 +0200

    nyanCat Plugin ergänzt

---
+++ plugin-src/nyanCat.ts
@@ -0,0 +1,74 @@
+import { PopupWidget, coreUtil, renderManager } from '../dist/pluginFacade';
+
+function performHelloWorld(): void {
+	PopupWidget.newAndRender({
+		title: 'Hello World!',
+		content: {
+			type: 'div',
+			style: {color: 'lightgreen', cursor: 'pointer'},
+			onclick: () => console.log('Hello World!'),
+			children: 'Hello, nice to see you!'
+		},
+		onClose: () => console.log('Have a nice day!')
+	})
+}
+
+const audio = new Audio('https://files.voicy.network/public/Content/Clips/Sound/c9243b3a-5a05-432e-88a8-48e4c5f9c93e.mp3')
+audio.loop = true;
+let isPlaying = false;
+
+const a = async () => {
+	const nyancatId = 'nyancat' + coreUtil.generateId();
+	const top = Math.random() * 100;
+	const topEnd = Math.random() * 100;
+	await renderManager.addElementTo('body', {
+		type: 'div',
+		id: nyancatId,
+		style: {
+			position: 'fixed',
+			top: `${top}%`,
+			transition: 'all 3s linear 0s',
+			left: '-20%',
+			transform: 'rotate(0deg)'
+		},
+		innerHTML: '<img style="height: 200px; width: 300px" src="https://media.tenor.com/-AyTtMgs2mMAAAAi/nyan-cat-nyan.gif">',
+		onclick: () => {
+			if (!isPlaying) {
+				audio.play();
+				isPlaying = true;
+			}
+			for (let i = 0; i <= 100; i++) {
+
+				a();
+			}
+		}
+	});
+	
+	while (true) {
+		await coreUtil.wait(10);
+		await renderManager.setStyleTo(nyancatId, {
+			left: `100%`,
+			top: `${topEnd}%`,
+			transition: 'all 3s linear 0s',
+			transform: 'rotate(360deg)'
+		})
+		await coreUtil.wait(3000);
+		await renderManager.setStyleTo(nyancatId, {
+			top: `${top}%`,
+			left: `-20%`,
+			transition: 'all 0s',
+			transform: 'rotate(0deg)'
+		})
+	}
+	return;
+	let i = 0;
+	while(true) {
+		await renderManager.setStyleTo('nyancat', {
+			left: `${(i % 300) / 3}%`,
+		})
+		await coreUtil.wait(10);
+		i++;
+	}
+}
+
+a();
