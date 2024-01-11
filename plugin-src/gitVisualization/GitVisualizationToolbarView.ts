5dcd8897c8a3d87d1ebc96c09da42519ea1c1bbe
Anja.Kleebaum <anja.kleebaum@andrena.de>
2024-01-11 23:57:41 +0100

    enable zooming to changes when switching projects

--- plugin-src/gitVisualization/GitVisualizationToolbarView.ts
+++ plugin-src/gitVisualization/GitVisualizationToolbarView.ts
@@ -14,6 +14,7 @@ export class GitVisualizationToolbarView extends UltimateWidget implements Toolb
         super()
         onMapLoaded.subscribe(async () => {
             this.selectedCommits = []
+            this.isZoomingEnabled = true
             await this.render()
         })
     }
