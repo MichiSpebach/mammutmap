5886eab7a8046ee52b977a1dfed19d8c04b8ddee
Michi <michi.spebach@gmail.com>
2024-04-03 21:04:33 +0200

    add bundleLinks to box sidebar

--- plugin-src/linkBundler.ts
+++ plugin-src/linkBundler.ts
@@ -9,14 +9,194 @@ import { applicationMenu } from '../dist/core/applicationMenu/applicationMenu'
 import { MenuItemFile } from '../dist/core/applicationMenu/MenuItemFile'
 import * as bundler from './linkBundler/bundler'
 import * as pluginFacade from '../dist/pluginFacade'
-import { ProgressBarWidget, coreUtil } from '../dist/pluginFacade'
+import { FolderBox, ProgressBarWidget, RenderElement, coreUtil, renderManager } from '../dist/pluginFacade'
 import * as knotMerger from './linkBundler/knotMerger'
 import { HighlightPropagatingLink } from './linkBundler/HighlightPropagatingLink'
+
+pluginFacade.overrideLink(HighlightPropagatingLink)
 
 contextMenu.addLinkMenuItem((link: Link) => new MenuItemFile({label: 'try to bundle', click: () => bundler.bundleLink(link)}))
 contextMenu.addLinkNodeMenuItem((node: NodeWidget) => new MenuItemFile({label: 'try to merge', click: () => knotMerger.mergeKnot(node)}))
 
-pluginFacade.overrideLink(HighlightPropagatingLink)
+Box.Sidebar.BasicToolkit.add({
+	topic: 'links',
+	indexWithinTopic: 1,
+	build: (box: Box) => Box.Sidebar.BasicToolkit.buildButton('bundle links...', () => openDialogForBundleLinks(box))
+})
+
+async function openDialogForBundleLinks(box: Box): Promise<void> {
+	const modeGroupId: string = coreUtil.generateId()
+	const modeAutoMaintained: string = coreUtil.generateId()
+	const modeAllId: string = coreUtil.generateId()
+	const borderingLinksId: string = coreUtil.generateId()
+	const managedLinksId: string = coreUtil.generateId()
+	const recursivelyId: string = coreUtil.generateId()
+	const pathsToIgnoreId: string = coreUtil.generateId()
+	const content: RenderElement[] = [
+		{
+			type: 'div',
+			innerHTML: `<input type="radio" id="${modeAutoMaintained}" name="${modeGroupId}" checked><label for="${modeAutoMaintained}">autoMaintained</label>`
+		},
+		{
+			type: 'div',
+			innerHTML: `<input type="radio" id="${modeAllId}" name="${modeGroupId}" ><label for="${modeAllId}">all</label>`
+		},
+		{
+			type: 'div',
+			innerHTML: `<input type="checkbox" id="${borderingLinksId}" checked><label for="${borderingLinksId}">bordering</label>`
+		},
+		{
+			type: 'div',
+			style: {marginTop: '4px'},
+			innerHTML: `<input type="checkbox" id="${managedLinksId}" checked><label for="${managedLinksId}">managed</label>`
+		}
+	]
+	const boxIsFolder: boolean = box instanceof FolderBox
+	if (boxIsFolder) {
+		content.push(
+			{
+				type: 'div',
+				style: {marginTop: '4px'},
+				innerHTML: `<input type="checkbox" id="${recursivelyId}"><label for="${recursivelyId}">recursively (This may take a while, depending on how many files there are)</label>`
+			},
+			{
+				type: 'div',
+				style: {display: 'flex'},
+				children: [
+					{
+						type: 'span',
+						children: 'if recursively, paths to ignore: '
+					},
+					{
+						type: 'input',
+						id: pathsToIgnoreId,
+						style: {flexGrow: '1', marginLeft: '4px'},
+						value: 'map, .git, node_modules, venv, .venv, .mvn, target, dist, out'
+					}
+				]
+			}
+		)
+	}
+	content.push({
+		type: 'button',
+		style: {marginTop: '4px'},
+		children: 'Start',
+		onclick: async () => {
+			bundleLinksRecursively(box, await promiseAllOfObject({
+				mode: renderManager.getCheckedOf(modeAllId).then(modeAllChecked => modeAllChecked ? 'all' : 'autoMaintained'), 
+				bordering: renderManager.getCheckedOf(borderingLinksId),
+				managed: renderManager.getCheckedOf(managedLinksId),
+				recursively: boxIsFolder ? renderManager.getCheckedOf(recursivelyId) : false,
+				pathsToIgnoreIfRecursively: boxIsFolder ? renderManager.getValueOf(pathsToIgnoreId).then(pathsToIgnore => pathsToIgnore.split(',').map(path => path.trim())) : []
+			}))
+			popup.unrender()
+		}
+	})
+	const popup: pluginFacade.PopupWidget = await pluginFacade.PopupWidget.newAndRender({title: 'Bundle Links', content})
+}
+
+async function promiseAllOfObject<T extends Object>(obj: T): Promise<{
+	[key in keyof T]: Awaited<T[key]>
+}> {
+	const entries = Object.entries(obj).map(async ([key, value]) => [key, await value])
+	return Object.fromEntries(await Promise.all(entries))
+}
+
+async function bundleLinksRecursively(box: Box, options: {
+	mode: 'autoMaintained'|'all'
+	bordering: boolean
+	managed: boolean
+	recursively: boolean
+	pathsToIgnoreIfRecursively: string[]
+}): Promise<void> {
+	console.info(`Start bundling links of '${box.getSrcPath()}' with options '${JSON.stringify(options)}'...`)
+	const progressBar: ProgressBarWidget = await ProgressBarWidget.newAndRenderInMainWidget()
+	let countingFinished: boolean = false
+	let fileCount: number = 1
+	let linkCount: number = 0
+	let processedFileCount: number = 0
+	let processedLinkCount: number = 0
+	let skippedLinkCount: number = 0
+	const counting: Promise<void> = count()
+
+	if (options.bordering) {
+		await bundleLinksSequentially(box.borderingLinks.getAll())
+	}
+	if (options.managed) {
+		await bundleLinksSequentially(box.links.getLinks())
+	}
+	processedFileCount++
+	if (options.recursively && box instanceof FolderBox) {
+		const iterator = new pluginFacade.BoxDepthTreeIterator(box, {srcPathsToIgnore: options.pathsToIgnoreIfRecursively})
+		while (await iterator.hasNextOrUnwatch()) {
+			const next: Box = await iterator.next()
+			if (next === box) {
+				continue
+			}
+			await bundleLinksSequentially(next.links.getLinks())
+			processedFileCount++
+			updateProgressBar()
+		}
+	}
+
+	await counting
+	await progressBar.finishAndRemove()
+	console.info(`Finished ${buildProgressText()}.`)
+
+	async function count(): Promise<void> {
+		if (options.bordering) {
+			linkCount += box.borderingLinks.getAll().length
+		}
+		if (options.managed) {
+			linkCount += box.links.getLinks().length
+		}
+		updateProgressBar()
+		if (options.recursively && box instanceof FolderBox) {
+			const iterator = new pluginFacade.BoxDepthTreeIterator(box, {srcPathsToIgnore: options.pathsToIgnoreIfRecursively})
+			while (await iterator.hasNextOrUnwatch()) {
+				const next: Box = await iterator.next()
+				if (next === box) {
+					continue
+				}
+				fileCount++
+				linkCount += next.links.getLinks().length
+				updateProgressBar()
+			}
+		}
+		countingFinished = true
+	}
+
+	async function bundleLinksSequentially(links: Link[]): Promise<void> {
+		for (const link of links) {
+			await bundleLink(link)
+		}
+	}
+
+	async function bundleLink(link: Link): Promise<void> {
+		if (!link.getManagingBox().links.getLinks().includes(link)) {
+			console.warn(`linkBundler::bundleLink(link: "${link.describe()}") link was removed in meanwhile.`)
+			return
+		}
+		if (options.mode === 'all' || link.isAutoMaintained()) {
+			await bundler.bundleLink(link, {unwatchDelayInMs: 500}).catch((reason) => console.warn(reason))
+		} else {
+			skippedLinkCount++
+		}
+		processedLinkCount++
+		updateProgressBar()
+	}
+
+	async function updateProgressBar(): Promise<void> {
+		const percent: number|undefined = countingFinished ? processedLinkCount/linkCount * 100 : undefined
+		const percentText: string = percent ? ` (${Math.round(percent*100)/100}%)` : ''
+		await progressBar.set({text: buildProgressText()+percentText, percent})
+	}
+
+	function buildProgressText(): string {
+		const skippedText = skippedLinkCount ? ` (skipped/not autoMaintained ${skippedLinkCount})`: ''
+		return `bundling links: file ${processedFileCount} of ${fileCount}, link ${processedLinkCount} of ${linkCount}${skippedText}`
+	}
+}
 
 let bundleNewLinksActivated: boolean = false
 let addLinkBackup: ((options: any) => Promise<Link>) = BoxLinks.prototype.add
@@ -59,20 +239,6 @@ function deactivateBundleNewLinks(): void {
 	bundleNewLinksActivated = false
 	BoxLinks.prototype.add = addLinkBackup
 	console.info(`bundleNewLinks deactivated`)
-}
-
-// TODO add a method like this to LinkEnd::getTargetAndRenderIfNecessary()?
-async function getLinkEndNode(link: Link, end: 'from'|'to'): Promise<{
-	node: Box|NodeWidget
-	watcher: BoxWatcher
-}> {
-	const linkEndPath: WayPointData[] = link.getData()[end].path
-	if (linkEndPath.length === 1 && linkEndPath[0].boxId === link.getManagingBox().getId()) {
-		return {node: link.getManagingBox(), watcher: await BoxWatcher.newAndWatch(link.getManagingBox())}
-	}
-	return link.getManagingBox().getDescendantByPathAndRenderIfNecessary(linkEndPath.map(wayPoint => {
-		return {id: wayPoint.boxId}
-	}))
 }
 
 const queue: {linkId: string, managingBoxSrcPath: string}[] = []
